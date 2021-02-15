c182e1e5bbbbf21989e4d52307011eab014a22e8
srouchanthorn <srouchanthorn@gmail.com>
2021-02-15 17:45:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 import { sleep } from 'k6';
 import http from 'k6/http';
 export let options = {
-  duration: '1m',
-  vus: 50,
+ stages: [
+    { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes
+    { duration: '5m', target: 0 }, // ramp-down to 0 users
+  ],
   thresholds: {
     http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
   },
